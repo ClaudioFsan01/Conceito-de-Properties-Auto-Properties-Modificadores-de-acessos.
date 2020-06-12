31b4af2849a335359b2d9b7355944a798f032ca6
Claudio Felix <claudio.felixdossantos@yahoo.com.br>
2020-06-12 16:51:48 -0300

    Conceito de Properties, Auto Properties, Modificadores de acessos

---
+++ ConceitosBasicos2/classes/Principal.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConceitosBasicos2
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Produto p = null;
+            int op = 0;
+
+            do
+            {
+                op = Menu();
+                switch (op)
+                {
+                    case 1:
+                        try
+                        {
+                            CadastrarProduto(p);
+                        }
+                        catch (ArgumentNullException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        catch (ArgumentException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }                       
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                       
+
+                        break;
+                    case 2:
+
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine(" Programa encerrado ! \n");
+                        break;
+                }
+
+
+            } while (op != 4);
+
+            Console.Read();
+
+        }
+
+
+        public static int Menu()
+        {
+            Console.WriteLine(" Menu de Opções : \n" +
+                "1- Cadastrar Produto : \n" +
+                "2- Adicionar Produto : \n" +
+                "3- Remover Produto : \n" +
+                "4- Sair \n");
+
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void CadastrarProduto(Produto p)
+        {
+           
+                Console.WriteLine(" Entre com os dados do Produto \n");
+
+              
+                Console.WriteLine(" Entre com o preço do produto :");
+                double preco = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                // p.SetPreco(Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+                Console.WriteLine(" Entre com a quantidade do estoque do produto :");
+                int qtdEstoque = int.Parse(Console.ReadLine());
+                //p.SetQtdEmEstoque(int.Parse(Console.ReadLine()));
+
+                p = new Produto(preco, qtdEstoque);
+
+               Console.WriteLine(" Entre com o nome do produto :");
+               p.Nome = Console.ReadLine();
+
+            // p.SetNome(Console.ReadLine());
+
+            /* Sintaxe alternativa para criação de objeto sem utilizar os construtores 
+             informando diretamente os valores 
+            p = new Produto { Nome = " ventilador", Preco = 30, QtdEstoque = 2 }; */
+
+            //MostrarProduto(p);
+            ExibirProduto(p);
+                AdicionarProduto(p);
+                RemoverProduto(p);
+           
+           
+        }
+
+        public static void AdicionarProduto(Produto p)
+        {
+            Console.WriteLine(" Entre com a quantidade que será adicionada ao estoque : \n");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            //MostrarProduto(p);
+            ExibirProduto(p);
+
+        }
+
+        public static void RemoverProduto(Produto p)
+        {
+            Console.WriteLine(" Entre com a quantidade que será removida do estoque : \n");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+            MostrarProduto(p);
+        }
+       
+         public static void ExibirProduto(Produto p)
+         {
+            Console.WriteLine("Dados do Produto : \n");
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.QtdEstoque);
+            //Console.Write("\n");
+
+
+
+        }
+
+
+        public static void MostrarProduto(Produto p)
+        {
+            Console.WriteLine(" Produto :" + p);
+            Console.WriteLine(" \n");
+        }
+    }
+}
+
---
+++ ConceitosBasicos2/classes/Produto.cs
@@ -0,0 +1,149 @@
+﻿/*Conceito de Properties, Auto Properties, Modificadores de acessos.
+ * Propriedades
+• São definições de métodos encapsulados, porém expondo uma
+sintaxe similar à de atributos e não de métodos
+
+Uma propriedade é um membro que oferece um mecanismo flexível para ler,
+gravar ou calcular o valor de um campo particular. As propriedades podem ser
+usadas como se fossem atributos públicos, mas na verdade elas são métodos
+especiais chamados "acessadores". Isso permite que os dados sejam
+acessados facilmente e ainda ajuda a promover a segurança e a flexibilidade
+dos métodos.
+ 
+  */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConceitosBasicos2
+{
+    class Produto
+    {
+        // Conceito de encapsulamento
+
+        //DECLARAÇÃO DE PROPRIEDADES COM AUTOIMPLEMENTED
+        //É uma forma simplificada de se declarar propriedades que não
+        //necessitam lógicas particulares para as operações get e set.  ex:
+       
+        private String _nome;
+        //private double _preco;
+        public double Preco { get; private set; }
+        //private int _qtdEstoque;
+        public int QtdEstoque { get; private set; }
+
+    // Por convençaõ da linguagem c# atributos privados utilizam este padrão  _nome
+
+    //Construtores 
+
+    // Referenciando outro Construtor em um Construtor com o uso da palavra this
+
+    public Produto() //Construtor padrão
+    {
+            //_qtdEstoque = 0;
+            /*por padrão os atributos já recebem o valor zero*/
+            QtdEstoque = 0;
+    }
+
+    public Produto(String nome, double preco) : this() /*  this() referencia o construtor padrão*/
+    {
+        _nome = nome;
+            //_preco = preco;
+            Preco = preco;
+    }
+
+    public Produto(String nome, double preco, int qtdEstoque) : this(nome, preco) /*  this(nome, preco) referencia o construtor Produto(String nome, double preco) */
+    {
+            //_qtdEstoque = qtdEstoque;
+            QtdEstoque = qtdEstoque;
+    }
+
+     public Produto(double preco, int qtdEstoque)
+     {
+            if(preco>0 && qtdEstoque >= 0)
+            {
+                //_preco = preco;
+                //_qtdEstoque = qtdEstoque;
+                Preco = preco;
+                QtdEstoque = qtdEstoque;
+            }
+            else
+            {
+                Console.WriteLine(" Preco ou QtdEstoque com valor invalido ! ");
+            }
+
+     }
+                
+        // Properties customizada
+
+            public String Nome
+            {
+             get { return _nome; }
+              set
+              {
+                if(value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+                else
+                {
+                    Console.WriteLine(" Nome invalido !");
+                }
+
+ 
+              }
+            }
+
+       
+
+        //public double Preco { get { return _preco; } }
+
+        
+
+       // public int QtdEmEstoque { get { return _qtdEstoque; } }
+    
+
+    public double ValorTotalEmEstoque()
+    {
+        return Preco * QtdEstoque;
+    }
+
+    public void AdicionarProdutos(int quantidade)
+    {
+            QtdEstoque += quantidade;
+    }
+
+    public void RemoverProdutos(int quantidade)
+    {
+        if (quantidade <= QtdEstoque)
+        {
+                QtdEstoque -= quantidade;
+        }
+        else
+        {
+            Console.WriteLine(" Quantidade no estoque menor que a solicitada ! \n");
+        }
+
+
+    }
+
+    
+    public override string ToString() //Retorna uma cadeira de caracteres que representa o objeto atual 
+    {//Aqui definimos como o objeto será retornado na forma de String
+        return _nome +
+            ", $ " +
+            Preco.ToString("F2", CultureInfo.InvariantCulture) +
+            " " + QtdEstoque +
+            " unidade, total : $ " +
+            ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+    }
+
+    /* * ToString - converte um objeto para String  
+    * Utilizando o método ToString podemos escolher como o objeto será representado na forma de String*/
+
+}
+}
